f4e6198cf3a9b01fba775dabb96db9881743ef35
« <jeremy.bourde@gmail.com>
2015-12-02 11:38:41 +0100

    2eme chocolat

--- build/CMakeFiles/local/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOCAL_CONFIG_H
-#define LOCAL_CONFIG_H
-
-#define GAME_DATADIR "/usr/local/share/games/game"
-
-static constexpr float BOX2D_PIXELS_PER_METER = 32.0f;
-
-#endif // LOCAL_CONFIG_H
--- src/local/main.cc
+++
@@ -1,9 +0,0 @@
-#include "noise.h"
-
-int main() {
-	int size = 10;
-	Noise noisetest(size);
-	noisetest.Compute();
-	noisetest.to_String();
-return 0;
-}
