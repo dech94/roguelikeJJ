a36841fe9beb5c1f7dac91a560a1a6609068c71a
jwacquier <jules.wacquier@gmail.com>
2015-12-09 17:35:38 +0100

    add base character

---
+++ src/local/Character.h
@@ -0,0 +1,13 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <game/Entity.h>
+#include <game/EventManager.h>
+#include <game/ResourceManager.h>
+
+class Character: public game::Entity {
+public:
+
+};
+
+#endif //CHARACTER_H
