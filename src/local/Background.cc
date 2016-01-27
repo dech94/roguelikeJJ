d52b1b9d2467cded5534ef90ecec9dd74bd7a478
dech94 <jules.wacquier@gmail.com>
2016-01-27 17:33:27 +0100

    commit classes tiles add

---
+++ src/local/Background.cc
@@ -0,0 +1,54 @@
+#include "Background.h"
+
+namespace local
+{
+	/*explicit*/ Background::Background(const local::Heightmap& heightmap)
+	:Tilemap(heightmap)
+	{
+	
+	}
+
+	/*virtual*/ Background::~Background()
+	{
+
+	}
+
+	int Background::convert(float value)
+	{
+		int tile = 0;
+			if(value < -0.5)
+			{
+				tile = 0;
+			}
+			else if(value < -0.4)
+			{
+				tile = 1;
+			}
+			else if(value < -0.2)
+			{
+				tile = 2;
+			}
+			else if(value < -0.1)
+			{
+				tile = 3;
+			}
+			else if(value < 0)
+			{
+				tile = 4;
+			}
+			else if(value < 0.5)
+			{
+				tile = 5;
+			}
+			else if(value < 0.7)
+			{
+				tile = 7;
+			}
+			else
+			{
+				tile = 8;
+			}
+	return tile;
+	}
+}
+
